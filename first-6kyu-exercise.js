94acbe142b779920ccb78b78c9f653772b6f9322
DevFaulk <faulkdev@gmail.com>
2023-11-28 13:33:29 -0500

    adjust if statements

--- first-6kyu-exercise.js
+++ first-6kyu-exercise.js
@@ -1,20 +1,18 @@
 function solution(str) {
   let splitArray = [];
-  let stringLastLetter = str.slice(-1);
-  // stringLastLetter is the last letter in the loop
 
   for (let i = 0; i < str.length; i++) {
     if (i % 2 !== 0) {
       splitArray.push(str.slice(i - 1, i + 1));
     }
-    if (str[i] == stringLastLetter && i % 2 == 0) {
+    if (i + 1 == str.length) {
       splitArray.push(str.slice(i, i + 1) + '_');
     }
   }
   return splitArray;
 }
 
-let str = 'BPSjbAmqhttlpiUaBggNrulh';
+let str = 'abcdefg';
 
 console.log(solution(str));
 

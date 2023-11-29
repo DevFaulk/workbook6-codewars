3b471a1bfe9c6ec144dc7ce4d6a565d27bd93dc6
DevFaulk <faulkdev@gmail.com>
2023-11-29 15:44:58 -0500

    complete exercise

--- complex-javascript/calculate-average.js
+++ complex-javascript/calculate-average.js
@@ -1,4 +1,14 @@
 function findAverage(array) {
-    array.forEach(number)
+  let sum = 0;
+
+  if (array.length == 0) {
     return 0;
   }
+
+  array.forEach(function (value) {
+    sum += value;
+  });
+  return sum / array.length;
+}
+
+console.log(findAverage([1, 2, 3, 4]));

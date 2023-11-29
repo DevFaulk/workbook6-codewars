36e65bba630449da7fa75f3a24242fb8eb4b3561
DevFaulk <faulkdev@gmail.com>
2023-11-29 15:59:02 -0500

    make exercise

---
+++ complex-javascript/sum-of-positive.js
@@ -0,0 +1,12 @@
+function positiveSum(arr) {
+  
+}
+
+
+
+
+console.log(positiveSum([1, 2, 3, 4, 5]));
+console.log(positiveSum([1, -2, 3, 4, 5]));
+console.log(positiveSum([]));
+console.log(positiveSum([-1, -2, -3, -4, -5]));
+console.log(positiveSum([-1, 2, 3, 4, -5]));

627173612cde56dc468d1b2eb3c39bda102b8e58
DevFaulk <faulkdev@gmail.com>
2023-11-28 23:08:09 -0500

    correct count showing wrong result

--- counting-valleys.js
+++ counting-valleys.js
@@ -1,62 +1,40 @@
-const terrain = {
-    Y: {
-      crest: 1,
-      base: 0,
-      shallow: -1,
-      trench: -2,
-    },
-    X: 0,
-  };
-  
-  let player = {
-    name: 'Gary',
-    Y: 0,
-    X: 0,
-  };
-  
-  function createDirections(directionArray) {
-    let newArray = [];
-  
-    for (let i = 0; i < directionArray.length; i++) {
-      const letter = directionArray[i];
-      if (letter == 'U') {
-        newArray.push('Up');
-      } else if (letter == 'D') {
-        newArray.push('Down');
-      } else if (letter == 'F') {
-        newArray.push('Forward');
-      }
+function countingValleys(str) {
+  let count = 0;
+  let altitude = 0;
+  let directionArray = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const letter = str[i];
+    if (letter == 'U') {
+      directionArray.push('Up');
+    } else if (letter == 'D') {
+      directionArray.push('Down');
+    } else if (letter == 'F') {
+      directionArray.push('Forward');
     }
-  
-    countingValleys(newArray);
   }
-  
-  function countingValleys(directionArray) {
-    let count = 0;
-  
-    for (const step of directionArray) {
-      switch (step) {
-        case 'Up':
-          switch (player.Y) {
-            case terrain.Y.shallow:
-              player.Y += 1;
-              if (player.Y === terrain.Y.base) {
-                count += 1; // Increment count when reaching the base after a valley
-              }
-              break;
-            default:
-              player.Y += 1;
+
+  for (const step of directionArray) {
+    switch (step) {
+      case 'Up':
+        if (altitude === -1) {
+            count += 1;
           }
-          break;
-        case 'Down':
-          player.Y -= 1;
-          break;
-        case 'Forward':
-          player.X += 1;
-      }
+        altitude += 1;
+        break;
+      case 'Down':
+        altitude -= 1;
+        break;
+      case 'Forward':
+        break;
     }
-  
-    console.log(`Number of valleys: ${count}`);
   }
-  
-  createDirections('UFFDDFDUDFUFUDUDUDUDU');
+
+  return count;
+}
+
+console.log(countingValleys('UFFDDFDUDFUFUDFFFD'));
+console.log(countingValleys('UFFDDFDUDFUFUUFFDDFDUDFUFU'));
+console.log(
+  countingValleys('UFFDDFDUDFUFUUFFDDFDUDFUFUUFFDDFDUDFUFUUFFDDFDUDFUFU')
+);

190d346bcab8ceac6752944959600455a388d595
DevFaulk <faulkdev@gmail.com>
2023-11-29 16:07:32 -0500

    append forEach for array

--- complex-javascript/sum-of-positive.js
+++ complex-javascript/sum-of-positive.js
@@ -1,10 +1,14 @@
 function positiveSum(arr) {
-  
+  arr.forEach(function (num, i) {
+    if (num[i] + num[i] == 0) {
+      arr.splice(i, 1);
+    } else if (num[i] == ' ') {
+      arr.splice(i, 1);
+    }
+  });
 }
 
-
-
-
+// test cases
 console.log(positiveSum([1, 2, 3, 4, 5]));
 console.log(positiveSum([1, -2, 3, 4, 5]));
 console.log(positiveSum([]));

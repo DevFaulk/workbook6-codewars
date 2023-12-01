819e5ee4a0d546551b3c5341dc7c0cbaff7962a6
DevFaulk <faulkdev@gmail.com>
2023-12-01 11:22:55 -0500

    complete exercise

--- complex-javascript/power-of-two.js
+++ complex-javascript/power-of-two.js
@@ -4,5 +4,10 @@ function isPowerOfTwo(n) {
   }
   if (n < 1) {
     return false;
-  }
+  } 
+  else return isPowerOfTwo(n / 2);
 }
+
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(4096));
+console.log(isPowerOfTwo(5));
